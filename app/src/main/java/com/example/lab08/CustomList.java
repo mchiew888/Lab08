ea2e3adb71733894612cb89b2be2ba2dc3e01766
mchiew888 <m.chiew.888@gmail.com>
2026-03-04 22:19:45 -0700

    delete and count implemented

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,12 +18,12 @@ public class CustomList {
         return cities.contains(city);
     }
 
-//    public void deleteCity(City city) {
-//        cities.remove(city);
-//    }
-//
-//    public int countCities() {
-//        return cities.size();
-//    }
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
+
+    public int countCities() {
+        return cities.size();
+    }
 
 }
